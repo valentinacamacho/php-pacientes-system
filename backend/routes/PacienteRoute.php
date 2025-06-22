15dba18d6be7e6554e41975ac76328dc6d32c18f
valentinacamacho <valentinacamacho0206@gmail.com>
2025-06-21 20:44:45 -0500

    feat: GET and POST routes are handled to list and register patients via controller.

--- backend/routes/PacienteRoute.php
+++ backend/routes/PacienteRoute.php
@@ -3,8 +3,22 @@ require_once __DIR__ . '/../controllers/PacienteController.php';
 
 $controller = new PacienteController();
 
-if(isset($_GET['id'])){
-    $controller->show($_GET['id']);
-}else{
-$controller->index();
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        if (isset($_GET['id'])){
+            $controller->show($_GET['id']);
+        } else {
+            $controller->index();
+        }
+        break;
+
+    case 'POST':
+        $controller->store();
+        break;
+
+       default:
+        http_response_code(405);
+        echo json_encode(["error" => "Método no permitido"]);
+        break;
 }
+
